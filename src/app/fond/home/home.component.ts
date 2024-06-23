9ba98a1ec7567f3da171f32a6dc86d947e7ab61a
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2024-06-23 22:57:27 +0300

    Add files via upload

--- src/app/fond/home/home.component.ts
+++ src/app/fond/home/home.component.ts
@@ -2,17 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { BrowserModule } from "@angular/platform-browser";
 import {CommonModule, ViewportScroller} from "@angular/common";
-import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    NgbModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
